63c33ba1b6ad593004ec52a30798af2a3b7c787f
Owen Hayes <owendthayes@gmail.com>
2026-08-10 20:36:14 +0100

    UI Overhaul in progress, added rounded corners to form border for aesthetics. Main functionality and navigation still to come.

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -31,45 +31,32 @@ namespace SleepyTime_2._0
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.imgHeaderDivider = new System.Windows.Forms.PictureBox();
             this.lblCurrentTime = new System.Windows.Forms.Label();
             this.tmrMain = new System.Windows.Forms.Timer(this.components);
             this.lblTitle = new System.Windows.Forms.Label();
-            this.txtHrs = new System.Windows.Forms.TextBox();
-            this.txtMins = new System.Windows.Forms.TextBox();
-            this.txtSecs = new System.Windows.Forms.TextBox();
-            this.cmbOperations = new System.Windows.Forms.ComboBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.imgLogo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHeaderDivider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // imgHeaderDivider
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
-            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(805, 64);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.White;
-            this.pictureBox2.Location = new System.Drawing.Point(-5, 64);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(805, 24);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.imgHeaderDivider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(48)))), ((int)(((byte)(75)))));
+            this.imgHeaderDivider.Location = new System.Drawing.Point(-3, 64);
+            this.imgHeaderDivider.Name = "imgHeaderDivider";
+            this.imgHeaderDivider.Size = new System.Drawing.Size(803, 10);
+            this.imgHeaderDivider.TabIndex = 1;
+            this.imgHeaderDivider.TabStop = false;
             // 
             // lblCurrentTime
             // 
             this.lblCurrentTime.AutoSize = true;
             this.lblCurrentTime.BackColor = System.Drawing.Color.White;
             this.lblCurrentTime.Font = new System.Drawing.Font("JetBrains Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCurrentTime.Location = new System.Drawing.Point(0, 67);
+            this.lblCurrentTime.Location = new System.Drawing.Point(261, 265);
             this.lblCurrentTime.Name = "lblCurrentTime";
             this.lblCurrentTime.Size = new System.Drawing.Size(105, 16);
             this.lblCurrentTime.TabIndex = 3;
@@ -81,109 +68,74 @@ namespace SleepyTime_2._0
             // 
             // lblTitle
             // 
-            this.lblTitle.BackColor = System.Drawing.Color.Purple;
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(31)))), ((int)(((byte)(48)))));
             this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblTitle.Font = new System.Drawing.Font("JetBrains Mono", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(800, 64);
             this.lblTitle.TabIndex = 4;
             this.lblTitle.Text = "SleepyTime";
-            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseDown);
             this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseMove);
             this.lblTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseUp);
             // 
-            // txtHrs
+            // imgLogo
             // 
-            this.txtHrs.Location = new System.Drawing.Point(82, 156);
-            this.txtHrs.Name = "txtHrs";
-            this.txtHrs.Size = new System.Drawing.Size(100, 23);
-            this.txtHrs.TabIndex = 5;
+            this.imgLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(31)))), ((int)(((byte)(48)))));
+            this.imgLogo.Image = global::SleepyTime_2._0.Properties.Resources.SleepyTimeImg;
+            this.imgLogo.Location = new System.Drawing.Point(306, 4);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Size = new System.Drawing.Size(61, 58);
+            this.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgLogo.TabIndex = 9;
+            this.imgLogo.TabStop = false;
             // 
-            // txtMins
+            // pictureBox1
             // 
-            this.txtMins.Location = new System.Drawing.Point(82, 185);
-            this.txtMins.Name = "txtMins";
-            this.txtMins.Size = new System.Drawing.Size(100, 23);
-            this.txtMins.TabIndex = 6;
-            // 
-            // txtSecs
-            // 
-            this.txtSecs.Location = new System.Drawing.Point(82, 214);
-            this.txtSecs.Name = "txtSecs";
-            this.txtSecs.Size = new System.Drawing.Size(100, 23);
-            this.txtSecs.TabIndex = 7;
-            // 
-            // cmbOperations
-            // 
-            this.cmbOperations.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cmbOperations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbOperations.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.cmbOperations.FormattingEnabled = true;
-            this.cmbOperations.Items.AddRange(new object[] {
-            "Shutdown",
-            "Restart",
-            "Lock Computer",
-            "Sleep"});
-            this.cmbOperations.Location = new System.Drawing.Point(254, 155);
-            this.cmbOperations.Name = "cmbOperations";
-            this.cmbOperations.Size = new System.Drawing.Size(121, 23);
-            this.cmbOperations.TabIndex = 8;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Purple;
-            this.pictureBox3.Image = global::SleepyTime_2._0.Properties.Resources.SleepyTimeImg;
-            this.pictureBox3.Location = new System.Drawing.Point(4, 2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(61, 58);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(48)))), ((int)(((byte)(75)))));
+            this.pictureBox1.Location = new System.Drawing.Point(134, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 386);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Thistle;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(31)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.cmbOperations);
-            this.Controls.Add(this.txtSecs);
-            this.Controls.Add(this.txtMins);
-            this.Controls.Add(this.txtHrs);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.lblCurrentTime);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCurrentTime);
+            this.Controls.Add(this.imgLogo);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.imgHeaderDivider);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SleepyTime";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.imgHeaderDivider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox imgHeaderDivider;
         private System.Windows.Forms.Label lblCurrentTime;
         private System.Windows.Forms.Timer tmrMain;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.TextBox txtHrs;
-        private System.Windows.Forms.TextBox txtMins;
-        private System.Windows.Forms.TextBox txtSecs;
-        private System.Windows.Forms.ComboBox cmbOperations;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox imgLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -9,11 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
 
 namespace SleepyTime_2._0
 {
     public partial class frmMain : Form
     {
+        //rounded borders values
+        private int borderRadius = 30, BorderSize = 2;
+        private Color boderColour = Color.Yellow;
+
         //Drag and Drop functionality for form header.
         private bool Dragging = false;
         public const int WM_NCLBUTTONDOWN = 0xA1;
@@ -28,6 +33,11 @@ namespace SleepyTime_2._0
         public frmMain()
         {
             InitializeComponent();
+
+            //further options for rounded form borders
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(BorderSize);
+
 
             tmrMain.Start();
         }
@@ -53,7 +63,7 @@ namespace SleepyTime_2._0
         {
             if (Dragging)
             {
-                if(e.Button == MouseButtons.Left)
+                if (e.Button == MouseButtons.Left)
                 {
                     ReleaseCapture();
                     SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
@@ -61,12 +71,62 @@ namespace SleepyTime_2._0
             }
         }
 
+        //methods for form rounded borders
+        private GraphicsPath GetRoundedPath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        //draw the rounded borders ready to be displayed.
+        private void FormRegionAndBorder(Form form, float radius, Graphics graph, Color borderColour, float borderSize)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                using (GraphicsPath roundPath = GetRoundedPath(form.ClientRectangle, radius))
+                using (Pen penBorder = new Pen(borderColour, borderSize)) 
+                using (Matrix transform = new Matrix())
+                {
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    form.Region = new Region(roundPath);
+                    if (borderSize >= 1)
+                    {
+                        Rectangle rect = form.ClientRectangle;
+                        float scaleX = 1.0F - ((borderSize + 1) / rect.Width);
+                        float scaleY = 1.0F - ((borderSize + 1) / rect.Height);
+
+                        transform.Scale(scaleX, scaleY);
+                        transform.Translate(borderSize / 1.6F, borderSize / 1.6F);
+
+                        graph.Transform = transform;
+                        graph.DrawPath(penBorder, roundPath);
+
+                    }
+                }
+
+            }
+        }
+
+        //draw rounded borders.
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            FormRegionAndBorder(this, borderRadius, e.Graphics, Color.FromArgb(45, 31, 48), 2);
+        }
+
         private void frmMain_Load(object sender, EventArgs e)
         {
-            if (cmbOperations.Items.Count>0)
-            {
-                cmbOperations.SelectedIndex = 0;
-            }
+            //if (cmbOperations.Items.Count>0)
+            //{
+            //    cmbOperations.SelectedIndex = 0;
+            //}
         }
     }
 }
