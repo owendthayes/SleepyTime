e9faf0452d8999ea7c53fbe2e62d1755a1cbc97f
Owen Hayes <owendthayes@gmail.com>
2026-08-10 16:45:22 +0100

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace SleepyTime_2._0
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblCurrentTime = new System.Windows.Forms.Label();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtHrs = new System.Windows.Forms.TextBox();
+            this.txtMins = new System.Windows.Forms.TextBox();
+            this.txtSecs = new System.Windows.Forms.TextBox();
+            this.cmbOperations = new System.Windows.Forms.ComboBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(805, 64);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(-5, 64);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(805, 24);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblCurrentTime
+            // 
+            this.lblCurrentTime.AutoSize = true;
+            this.lblCurrentTime.BackColor = System.Drawing.Color.White;
+            this.lblCurrentTime.Font = new System.Drawing.Font("JetBrains Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCurrentTime.Location = new System.Drawing.Point(0, 67);
+            this.lblCurrentTime.Name = "lblCurrentTime";
+            this.lblCurrentTime.Size = new System.Drawing.Size(105, 16);
+            this.lblCurrentTime.TabIndex = 3;
+            this.lblCurrentTime.Text = "Current Time: ";
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Purple;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("JetBrains Mono", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(800, 64);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "SleepyTime";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseDown);
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseMove);
+            this.lblTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseUp);
+            // 
+            // txtHrs
+            // 
+            this.txtHrs.Location = new System.Drawing.Point(82, 156);
+            this.txtHrs.Name = "txtHrs";
+            this.txtHrs.Size = new System.Drawing.Size(100, 23);
+            this.txtHrs.TabIndex = 5;
+            // 
+            // txtMins
+            // 
+            this.txtMins.Location = new System.Drawing.Point(82, 185);
+            this.txtMins.Name = "txtMins";
+            this.txtMins.Size = new System.Drawing.Size(100, 23);
+            this.txtMins.TabIndex = 6;
+            // 
+            // txtSecs
+            // 
+            this.txtSecs.Location = new System.Drawing.Point(82, 214);
+            this.txtSecs.Name = "txtSecs";
+            this.txtSecs.Size = new System.Drawing.Size(100, 23);
+            this.txtSecs.TabIndex = 7;
+            // 
+            // cmbOperations
+            // 
+            this.cmbOperations.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbOperations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperations.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmbOperations.FormattingEnabled = true;
+            this.cmbOperations.Items.AddRange(new object[] {
+            "Shutdown",
+            "Restart",
+            "Lock Computer",
+            "Sleep"});
+            this.cmbOperations.Location = new System.Drawing.Point(254, 155);
+            this.cmbOperations.Name = "cmbOperations";
+            this.cmbOperations.Size = new System.Drawing.Size(121, 23);
+            this.cmbOperations.TabIndex = 8;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox3.Image = global::SleepyTime_2._0.Properties.Resources.SleepyTimeImg;
+            this.pictureBox3.Location = new System.Drawing.Point(4, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(61, 58);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.cmbOperations);
+            this.Controls.Add(this.txtSecs);
+            this.Controls.Add(this.txtMins);
+            this.Controls.Add(this.txtHrs);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblCurrentTime);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SleepyTime";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblCurrentTime;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtHrs;
+        private System.Windows.Forms.TextBox txtMins;
+        private System.Windows.Forms.TextBox txtSecs;
+        private System.Windows.Forms.ComboBox cmbOperations;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace SleepyTime_2._0
+{
+    public partial class frmMain : Form
+    {
+        //Drag and Drop functionality for form header.
+        private bool Dragging = false;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            tmrMain.Start();
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            lblCurrentTime.Text = ("Current Time: " + DateTime.Now.ToString("HH:mm"));
+        }
+
+
+        //drag and drop functionality for header of form.
+        private void lblTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            Dragging = true;
+        }
+
+        private void lblTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+        }
+
+        private void lblTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Dragging)
+            {
+                if(e.Button == MouseButtons.Left)
+                {
+                    ReleaseCapture();
+                    SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if (cmbOperations.Items.Count>0)
+            {
+                cmbOperations.SelectedIndex = 0;
+            }
+        }
+    }
+}
