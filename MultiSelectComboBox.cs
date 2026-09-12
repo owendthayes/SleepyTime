23dee09eaeb03954760539dca458fb6882b40e09
Owen Hayes <owendthayes@gmail.com>
2026-09-12 17:10:57 +0100

    Still researching how to implement mutli-select combo box, struggling a little.

---
+++ MultiSelectComboBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyTime_2._0
+{
+    class MultiSelectComboBox
+    {
+        public List<DayOfWeek> SelectedDays { get; }
+    }
+}
